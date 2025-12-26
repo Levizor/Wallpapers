c11e08c39772af2c3f5b5ae7b29fb476757a35ea
Levizor <levizor@disroot.org>
2025-12-26 14:37:16 +0100

    add flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,33 @@
 {
-  description = "Wallpaper packs flake";
+  description = "A collection of wallpapers.";
 
-  outputs =
-    { self, ... }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
     let
-      # Automatically detect all directories in the repo
-      packs = builtins.filter (d: builtins.pathExists d && builtins.isDirectory d) (
-        builtins.attrNames (builtins.readDir ./.)
-      );
+      lib = nixpkgs.lib;
 
-      # Map each pack name to its path
-      packPaths = builtins.listToAttrs (
-        map (d: {
-          name = d;
-          value = ./${d};
-        }) packs
-      );
+      # List of directories to exclude from wallpaper packs
+      excludedDirs = [ ".git" ".github" "lib" ];
+
+      # Get all entries in the root directory
+      entries = builtins.readDir ./.;
+
+      # Filter for directories that are wallpaper packs
+      isWallpaperPack = name: type: type == "directory" && !(lib.elem name excludedDirs);
+
+      # Get the names of the wallpaper pack directories
+      wallpaperPacks = lib.attrNames (lib.filterAttrs isWallpaperPack entries);
+
+      # Create an attribute set of wallpapers, where each key is a pack name
+      # and the value is a path to the directory in the Nix store.
+      wallpapers = lib.genAttrs wallpaperPacks (packName: ./. + "/${packName}");
 
     in
     {
-      # Each wallpaper pack accessible individually
-      wallpapers = packPaths;
-
-      # Optional: merged pack (symlink farm)
-      mergedPack = builtins.fromJSON (builtins.toJSON packs); # placeholder
+      # The primary output, providing access to individual wallpaper packs
+      inherit wallpapers;
     };
 }
